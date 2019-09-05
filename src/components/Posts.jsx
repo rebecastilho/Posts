c995e72565d109e89d27f815185534ee7f41a0fb
REBECA <becacastilhocamargo@gmail.com>
2019-09-05 20:35:07 -0300

    Salvar Posts e , uso da biblioteca axios

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,44 +1,93 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import objeto from '../posts.json';
 import Post from './Post';
+import axios from 'axios';
 
-class Posts extends React.Component{
-    constructor(props){
-      super(props)
-      this.state ={
-          posts :objeto.posts
-      }
-  
-      this._excluirPost = this._excluirPost.bind(this)
+class Posts extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: objeto.posts,
+      titulo : "",
+      url : ""
     }
-  
-_excluirPost(id) {
-   var newPosts = this.state.posts.filter((post) =>{
+
+    axios.get("http://www.mocky.io/v2/5d7193e0330000f7cf7799a1")
+    .then((result) => {
+      console.log(result.data.posts)
+      this.setState({
+        posts: result.data.posts
+      })
+    });
+
+    this._excluirPost = this._excluirPost.bind(this)
+  }
+
+  _excluirPost(id) {
+    var newPosts = this.state.posts.filter((post) => {
       return post.id != id;
-   })
+    })
 
-   this.setState({posts : newPosts})
-}
+    this.setState({ posts: newPosts })
+  }
 
-  render(){
-  return (
-    <div className="App">
-      {
-        this.state.posts.map((post, i)=>
-          <Post
-            key = {i}
-            id = {post.id}
-            url_imagem = {post.url}
-            titulo = {post.titulo}
-            _excluirPost = {this._excluirPost}
-           />
-
-        )
-      }
-    </div>
-  );
+  _salvarPost(){
+    var newId = this.state.posts[this.state.posts.length-1].id+1
+    var newPost =  {
+      id : newId,
+      titulo : this.state.titulo,
+      url : this.state.url
     }
+
+    var posts = this.state.posts
+    posts.push(newPost)
+
+    this.setState({
+      posts : posts,
+      titulo : "",
+      url : ""
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.posts.map((post, i) =>
+            <Post
+              key={i}
+              id={post.id}
+              url_imagem={post.url}
+              titulo={post.titulo}
+              _excluirPost={this._excluirPost}
+            />
+
+          )
+        }
+        <div>
+        <span>Titulo</span>
+        <input value ={this.state.titulo} onChange={(e) => {
+          this.setState({titulo : e.target.value})
+         }}>
+         </input>
+        </div>
+
+        <div>
+        <span>URL</span>
+        <input value ={this.state.url} onChange={(e) => {
+          this.setState({url : e.target.value})
+         }}>
+         </input>
+        </div>
+
+        <button class='btn btn-primary' onClick={() => { this._salvarPost() }} > Salvar </button>
+
+      </div>
+    );
+
+
+  }
 }
 
 export default Posts;
